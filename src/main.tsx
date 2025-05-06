8c6570776be7c230e50dbc1589a916b01499e6df
sousou1216 <sousou1216@yahoo.co.jp>
2025-05-06 10:40:05 +0900

    feat: Topページを作成

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import "./styles/output.css"
 import "./styles/destyle.css"
+import { ToPage } from './components/pages/ToPage'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1 className="text-3xl font-bold underline">Hello World!</h1>
+    <ToPage />
   </StrictMode>,
 )
