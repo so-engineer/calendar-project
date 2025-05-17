84343f91b247df318cd1e3a7951467941d5d2708
sousou1216 <sousou1216@yahoo.co.jp>
2025-05-17 15:14:12 +0900

    feat: dateListの各日付に予定を格納するようにした

--- src/components/pages/CalenderPage.tsx
+++ src/components/pages/CalenderPage.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react'
-import { eachDayOfInterval, eachWeekOfInterval, endOfMonth, endOfWeek, getMonth, startOfMonth } from 'date-fns'
+import { eachDayOfInterval, eachWeekOfInterval, endOfMonth, endOfWeek, getMonth, isSameDay, startOfMonth } from 'date-fns'
 import { CalendarHeader } from '../organisms/CalendarHeader'
 import { CalendarBody } from '../organisms/CalendarBody'
+import { DateList, Schedule } from '../../types/calendar'
+import { getScheduleList } from '../../api/calendar'
 
 export const CalenderPage = () => {
   const today = new Date()
-  const [dateList, setDateList] = useState<Date[][]>([])
+  const [dateList, setDateList] = useState<DateList>([])
 
 
   useEffect(() => {
@@ -14,11 +16,27 @@ export const CalenderPage = () => {
       end: endOfMonth(today),
     })
     // console.log(monthOfSundayList)
-    const newDateList: Date[][] = monthOfSundayList.map((date) => {
+    const newDateList: DateList = monthOfSundayList.map((date) => {
       return eachDayOfInterval({
         start: date,
         end: endOfWeek(date),
-      })
+      }).map((date) => ({date, schedules: [] as Schedule[]}))
+    })
+
+    const scheduleList = getScheduleList()
+    scheduleList.forEach((schedule) => {
+      const firstIndex = newDateList.findIndex((oneWeek) => 
+        oneWeek.some((item) => isSameDay(item.date, schedule.date))
+      )
+      if (firstIndex === -1) return
+      const secondIndex = newDateList[firstIndex].findIndex((item) => 
+        isSameDay(item.date, schedule.date)
+      )
+
+      newDateList[firstIndex][secondIndex].schedules = [
+        ...newDateList[firstIndex][secondIndex].schedules,
+        schedule,
+      ]
     })
     // console.log(newDateList)
     setDateList(newDateList)
