875ca9c123e9ec2aafeeda699183323357cbe5a8
sousou1216 <sousou1216@yahoo.co.jp>
2025-05-19 08:44:08 +0900

    feat: 予定詳細モーダルを作成

--- src/components/organisms/CalendarBody.tsx
+++ src/components/organisms/CalendarBody.tsx
@@ -1,7 +1,9 @@
 import { getDate } from 'date-fns'
 import { dateColor } from '../../libs/date'
-import { DateList } from '../../types/calendar'
+import { DateList, Schedule } from '../../types/calendar'
 import { ScheduleBtn } from '../atoms/ScheduleBtn'
+import { ScheduleDetailModal } from './ScheduleDetailModal'
+import { useState } from 'react'
 
 type PropsType = {
   currentDate: Date
@@ -9,29 +11,37 @@ type PropsType = {
 }
 
 export const CalendarBody = ({currentDate, dateList}: PropsType) => {
+  const [selectedSchedule, setSelectedSchedule] = useState<Schedule | null>(
+    null
+  )
+  const closeModal = () => setSelectedSchedule(null)
+
   return (
-    <tbody>
-      {dateList.map((oneWeek, weekIndex) => (
-        <tr key={`week-${weekIndex}`} className='mx-10'>
-          {oneWeek.map((item, dayIndex) => (
-            <td key={`day-${weekIndex}-${dayIndex}`} className='bg-white h-[10vh] border-2 border-solid border-lime-800'>
-              <span className={`inline-block w-[20px] leading-[20px] text-center ${dateColor(
-                  item.date,
-                  currentDate
-                )}`}>
-                {getDate(item.date)}
-              </span>
-              <div className='flex flex-col items-center gap-1 pb-2'>
-                {item.schedules.map((schedule) => (
-                  <ScheduleBtn key={schedule.id}>
-                    {schedule.title}
-                  </ScheduleBtn>
-                ))}
-              </div>
-            </td>
-          ))}
-        </tr>
-      ))}
-    </tbody>
+    <>
+      <tbody>
+        {dateList.map((oneWeek, weekIndex) => (
+          <tr key={`week-${weekIndex}`} className='mx-10'>
+            {oneWeek.map((item, dayIndex) => (
+              <td key={`day-${weekIndex}-${dayIndex}`} className='bg-white h-[10vh] border-2 border-solid border-lime-800'>
+                <span className={`inline-block w-[20px] leading-[20px] text-center ${dateColor(
+                    item.date,
+                    currentDate
+                  )}`}>
+                  {getDate(item.date)}
+                </span>
+                <div className='flex flex-col items-center gap-1 pb-2'>
+                  {item.schedules.map((schedule) => (
+                    <ScheduleBtn key={schedule.id} onClick={() => setSelectedSchedule(schedule)}>
+                      {schedule.title}
+                    </ScheduleBtn>
+                  ))}
+                </div>
+              </td>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+      <ScheduleDetailModal selectedSchedule={selectedSchedule} closeModal={closeModal} />
+    </>
   )
 }
